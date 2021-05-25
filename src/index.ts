2dad7d915f46278c2f5ae455f0b93dc0ae8de4bf
Chuan Lin <chuan.lin.cl@gmail.com>
2021-05-24 20:30:33 -0400

    added express

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,31 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from 'express'
+
 const main = async () => {
     // Connect to the database
     const orm = await MikroORM.init(microConfig)
+    // run in the CLI: npx mikro-orm migration:create
     // Run the migrations
     await orm.getMigrator().up();
-    // Creates an instance of post./Run SQL
-    const post = orm.em.create(Post, {title:'my first post'})
-    // const post = new Post({'my first post'})
-    // Pushes post to the database
-    await orm.em.persistAndFlush(post)
-    // console.log('-------------------sql2-------------------')
-    // await orm.em.nativeInsert(Post, {title: 'my first post 2'})
-    const posts = await orm.em.find(Post, {})
-    console.log(posts)
-    
+    const app = express();
+    app.get('/',(_,res)=>{
+        res.send("hello")
+    })
+    app.listen(4000, () => {
+        console.log('servr has started on localhost:4000');
+    })
+    // // Creates an instance of post./Run SQL
+    // const post = orm.em.create(Post, {title:'my first post'})
+    // // const post = new Post({'my first post'})
+    // // Pushes post to the database
+    // await orm.em.persistAndFlush(post)
+    // // console.log('-------------------sql2-------------------')
+    // // await orm.em.nativeInsert(Post, {title: 'my first post 2'})
+    // const posts = await orm.em.find(Post, {})
+    // console.log(posts)
+
 }
 
 main().catch((err)=>{
