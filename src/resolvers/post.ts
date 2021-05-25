dbcc91acb1fa86574cba3628ed79c19cb9255ba4
Chuan Lin <chuan.lin.cl@gmail.com>
2021-05-24 22:41:22 -0400

    added CRUD operations with mikroorm/graphql (getposts,getpost,createpost,updatepost,deletepost)

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -2,10 +2,11 @@ import { Post } from "../entities/Post";
 // Note that VSCode auto-complete uses "src/..."
 // use relative addressing from current directory (../...)
 import { MyContext } from "../types";
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int, Mutation} from "type-graphql";
 
 @Resolver()
 export class PostResolver{
+    // Multiple Posts
     @Query(()=>[Post])
     posts(
     //     @Ctx() ctx: MyContext
@@ -16,4 +17,57 @@ export class PostResolver{
     ): Promise<Post[]> {
         return em.find(Post, {})
     }
+    // Lookup by ID
+    // GraphQL type
+    // Setting nullable:true removes extra error information returned from a null response GraphQL query.
+    @Query(()=>Post,{nullable:true})
+    post(
+        // Can change 'id' and the changed value will be used in the GraphQL query.
+        @Arg("id",()=>Int) id: number,
+        @Ctx() {em}: MyContext
+        // Typescript type
+        ): Promise<Post | null> {
+        return em.findOne(Post, {id})
+    }
+    // Create Post
+    // type GraphQL may be infer types
+    // ( Arg("title",()=>String) => Arg("title"))
+    @Mutation(()=>Post)
+    async createPost(
+        @Arg("title",()=>String) title: string,
+        @Ctx() {em}: MyContext
+        // Typescript type
+        ): Promise<Post> {
+            const post = em.create(Post,{title})
+            await em.persistAndFlush(post)
+            return post;
+    }
+
+    // Update Post
+    @Mutation(()=>Post, {nullable:true})
+    async updatePost(
+        @Arg("id",() => String) id: number,
+        @Arg("title", () => String, {nullable:true}) title:string,
+        @Ctx() {em}: MyContext
+        ): Promise<Post | null> {
+            const post = await em.findOne(Post,{id});
+            if (!post){
+                return null
+            }if (typeof title !== 'undefined') {
+                post.title = title
+                await em.persistAndFlush(post)
+            }
+            await em.persistAndFlush(post)
+            return post;
+    }
+    // Delete Post
+    @Mutation(()=>Boolean)
+    async deletePost(
+        @Arg("id",() => String) id: number,
+        @Ctx() {em}: MyContext
+        ): Promise<boolean> {
+            em.nativeDelete(Post,{id})
+            return true;
+    }    
 }
+
